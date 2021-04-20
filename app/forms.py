63cb9f7d778bb3e0cefe16c6765e676580ff9ee5
Brian <bstanton773@gmail.com>
2021-04-19 20:38:37 -0500

    Working login logout connected to db

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
 
 class SearchMovieForm(FlaskForm):
     title = StringField('Movie Title', validators=[DataRequired()])
+    submit = SubmitField()
+
+class UserInfoForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_pass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField()
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
     submit = SubmitField()
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
-from app import app
-from flask import render_template, request
-from app.forms import SearchMovieForm
-import tmdbsimple as tmdb
 import os
+from app import app, db
+from flask import render_template, request, flash, redirect, url_for
+from flask_login import login_user, logout_user, login_required, current_user
+from app.forms import SearchMovieForm, UserInfoForm, LoginForm
+from app.models import User
+import tmdbsimple as tmdb
+from werkzeug.security import check_password_hash
 
 tmdb.API_KEY = os.environ.get('TMDB_API_KEY')
 
@@ -29,3 +32,56 @@ def search():
 def movie_detail(id):
     movie = tmdb.Movies(id).info()
     return render_template('movie_detail.html', movie=movie)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = UserInfoForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        # print(username, email, password)
+        
+        # create a new instance of User
+        new_user = User(username, email, password)
+        # add new instance to our database
+        db.session.add(new_user)
+        # commit database
+        db.session.commit()
+
+        # Send email to new user
+
+        flash("You have succesfully signed up!", "success")
+        return redirect(url_for('index'))
+    return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username).first()
+
+        if user is None or not check_password_hash(user.password, password):
+            flash("Incorrect Email/Password. Please try again", 'danger')
+            return redirect(url_for('login'))
+        
+        login_user(user, remember=form.remember_me.data)
+        flash("You have successfully logged in!", 'success')
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(url_for(next_page.lstrip('/')))
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash("You have succesfully logged out", 'primary')
+    return redirect(url_for('index'))
