2df47c86372fded55254e7f9f02676290de0dbf5
Brian <bstanton773@gmail.com>
2021-04-22 16:50:44 -0500

    Rating model

--- app/models.py
+++ app/models.py
@@ -14,11 +14,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
     watchlist = db.Column(db.ARRAY(db.Integer))
+    ratings = db.relationship('Rating', backref='user', lazy=True)
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f'<User {self.id} | {self.username} >'
 
     def add_to_watchlist(self, movie_id):
         if movie_id not in self.watchlist:
@@ -28,4 +32,16 @@ class User(db.Model, UserMixin):
     def remove_from_watchlist(self, movie_id):
         self.watchlist = [m_id for m_id in self.watchlist if m_id != movie_id]
         db.session.commit()
-    
+
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    movie_id = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __init__(self, user_id, movie_id, rating):
+        self.user_id = user_id
+        self.movie_id = movie_id
+        self.rating = rating
