5685ac3acfdad2817efea9f32205b748875a8bbf
Brian <bstanton773@gmail.com>
2021-04-22 19:06:50 -0500

    Default new user to have empty list for watchlist

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
-    watchlist = db.Column(db.ARRAY(db.Integer))
+    watchlist = db.Column(db.ARRAY(db.Integer), default=[])
     ratings = db.relationship('Rating', backref='user', lazy=True)
 
     def __init__(self, username, email, password):
